3f6f1a6ef65f21ed11f24e717a63bf11823e6459
Nguyễn Trần Quốc Duy <145688546+ntqduy@users.noreply.github.com>
2025-06-03 02:59:35 +0700

    Add files via upload
    app

---
+++ factorial .py
@@ -0,0 +1,7 @@
+def factorial(value):
+    if value < 0:
+        raise ValueError("Value must be a non-negative integer.")
+    if value == 0 or value == 1:
+        return 1
+    else:
+        return value * factorial(value - 1)
